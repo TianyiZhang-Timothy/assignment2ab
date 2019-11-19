fe5b70fde787addf495d993ee6b93c95d2520051
TianyiZhang <timothy.tudis@gmail.com>
2019-11-19 12:45:24 -0500

    the first page - student page

--- HTTP5101_School_System/StudentPage.aspx
+++ HTTP5101_School_System/StudentPage.aspx
@@ -1,15 +1,22 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentPage.aspx.cs" Inherits="HTTP5101_School_System.StudentPage" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Layout.Master" CodeBehind="StudentPage.aspx.cs" Inherits="HTTP5101_School_System.StudentPage" %>
 
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
+<asp:Content ID="student_page" ContentPlaceHolderID="body" runat="server">
+    <h1>Student</h1>
+    <div id="student_search_div">
+        <asp:label for="student_search" runat="server">Search:</asp:label>
+        <asp:TextBox ID="student_search" runat="server"></asp:TextBox>
+        <asp:Button runat="server" Text="submit" />
+        <div id="show_sql" runat="server"></div>
+    </div>
+    <div class="info_table" runat="server">
+        <div class="title">
+            <div class="col3">STUDENT NUMBER</div>
+            <div class="col3">FULL NAME</div>
+            <div class="col3last">ENROLMENT DATE</div>
         </div>
-    </form>
-</body>
-</html>
+        <div id="student_result" runat="server">
+
+        </div>
+    </div>
+
+</asp:Content>
--- HTTP5101_School_System/StudentPage.aspx.cs
+++ HTTP5101_School_System/StudentPage.aspx.cs
@@ -11,7 +11,40 @@ namespace HTTP5101_School_System
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string searchkey = "";
 
+            // query
+            string query = "select * from STUDENTS";
+
+            if (searchkey != "")
+            {
+                query += " WHERE STUDENTFNAME like '%" + searchkey + "%' ";
+                query += " or STUDENTLNAME like '%" + searchkey + "%' ";
+                query += " or STUDENTNUMBER like '%" + searchkey + "%' ";
+            }
+
+            var db = new SCHOOLDB();
+            List<Dictionary<String, String>> rs = db.List_Query(query);
+            foreach (Dictionary<String, String> row in rs)
+            {
+                student_result.InnerHtml += "<div class=\"listitem\">";
+
+                string studentid = row["STUDENTID"];
+
+                string studentfirstname = row["STUDENTFNAME"];
+                student_result.InnerHtml += "<div class=\"col4\"><a href=\"ShowStudent.aspx?studentid=" + studentid + "\">" + studentfirstname + "</a></div>";
+
+                string studentlastname = row["STUDENTLNAME"];
+                student_result.InnerHtml += "<div class=\"col4\">" + studentlastname + "</div>";
+
+                string studentnumber = row["STUDENTNUMBER"];
+                student_result.InnerHtml += "<div class=\"col4\">" + studentnumber + "</div>";
+
+                string enrolmentdate = row["ENROLMENTDATE"];
+                student_result.InnerHtml += "<div class=\"col4last\">" + enrolmentdate + "</div>";
+
+                student_result.InnerHtml += "</div>";
+            }
         }
     }
 }
